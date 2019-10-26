17b9204dffdf90dc741665fee9b8c47791979d51
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-10-25 18:16:02 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+		<title>Document</title>
+</head>
+
+<body>
+	<header>
+		<h1>JS crash course</h1>
+	</header>
+
+	<script src="scripts/main.js"></script>
+</body>
+
+</html>
