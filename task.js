de61f88d472f370f1b38249514ed6d51de1be515
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2020-02-07 13:13:26 -0700

    working on plural sight javascript

---
+++ task.js
@@ -0,0 +1,19 @@
+//how to create new objects
+
+var obj = {};
+
+var nextObj = Object.create(Object.prototype);
+
+var lastObj = new Object();
+
+//Assigning Keys and Values
+//Dot Notation
+
+var obj = {};
+
+obj.param = 'new value';
+
+console.log(obj.param); //new value
+
+
+
