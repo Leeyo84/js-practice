fdfaf371b5ded269cd2c3f5754c4bdb5bc895d44
Leonela <Leonela_Gutierrez@hotmail.com>
2020-02-18 18:26:54 -0700

    making sure git clone is working

--- task.js
+++ task.js
@@ -17,3 +17,19 @@ console.log(obj.param); //new value
 
 
 
+//how to create new objects
+
+var obj = {};
+
+var nextObj = Object.create(Object.prototype);
+
+var lastObj = new Object();
+
+//Assigning Keys and Values
+//Dot Notation
+
+var obj = {};
+
+obj.param = 'new value';
+
+console.log(obj.param); //new value
