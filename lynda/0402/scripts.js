99ecf881d839f0bdae42139376881301df130528
Leonela <Leonela_Gutierrez@hotmail.com>
2020-02-19 18:00:01 -0700

    making basic function reusable.

--- lynda/0402/scripts.js
+++ lynda/0402/scripts.js
@@ -17,12 +17,21 @@
 // basic function via lynda course
 
 function findBiggestFraction(a,b) {
-    a>b ? console.log("a: ", a) : console.log("b: ", b);
+    var result;
+    a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
+    return result;
 }
 
 var firstFraction = 3/4;
 var secondFraction = 5/7;
 
-findBiggestFraction(firstFraction, secondFraction);
-findBiggestFraction(7/16,13/25);
-findBiggestFraction(1/2,3/4);
+var fractionResult = findBiggestFraction(firstFraction, secondFraction);
+
+// console.log(fractionResult);
+// findBiggestFraction(7/16,13/25);
+// findBiggestFraction(1/2,3/4);
+
+console.log("First fraction result: ", firstFraction);
+console.log("Second fraction result: ", secondFraction);
+console.log("Fraction " + fractionResult[0] + "with a value of " + fractionResult[1] + " is the biggest!");
+
