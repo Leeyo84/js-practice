2fcc0de9fe0fd81f9b1d364264c8d472e346ebe9
Leonela <Leonela_Gutierrez@hotmail.com>
2020-02-19 17:39:26 -0700

    basic js function

--- lynda/0402/scripts.js
+++ lynda/0402/scripts.js
@@ -1,3 +1,27 @@
+// node server
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// basic function via lynda course
+
 function findBiggestFraction() {
-    console.log("The function is running!");
+    a>b ? console.log("a: ", a) : console.log("b: ", b);
 }
+
+var a = 3/4;
+var b = 5/7;
+
+findBiggestFraction();
